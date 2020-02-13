6d03f02a5f877edb52062923b269a8d866baddae
Gabor Szabo <gabor@szabgab.com>
2020-02-13 10:44:27 +0200

    saving products in a json database

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
+import os
+import json
 
 app = Flask(__name__)
+
+def get_db_file():
+    if 'TEST_DB' in os.environ:
+        return os.environ['TEST_DB']
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sl.json')
 
 @app.route('/')
 def main_get():
@@ -10,13 +17,21 @@ def main_get():
 
 @app.route('/', methods=["POST"])
 def main_post():
-    products = []
-
+    db_file = get_db_file()
+    if os.path.exists(db_file):
+        with open(db_file) as fh:
+            data = json.load(fh)
+    else:
+        data = {
+            'products': []
+        }
     if 'product' in request.form:
         product = request.form['product']
-        products.append(product)
+        data['products'].append(product)
+    with open(db_file, 'w') as fh:
+        json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
 
     return render_template('main.html',
                            title = "Shopping List",
-                           products = products,
+                           products = data['products'],
                     )
--- test_app.py
+++ test_app.py
@@ -1,10 +1,15 @@
 import app
+import os
 
-def test_app():
+def test_app(tmpdir):
+    print(tmpdir)
+    os.environ['TEST_DB'] = os.path.join(tmpdir, 'temp.db')
+
     web = app.app.test_client()
 
     rv = web.get('/')
     assert rv.status_code == 200
+    #print(rv.data)
     assert "Shopping List" in rv.data.decode('utf-8')
     assert "Apple juice" not in rv.data.decode('utf-8')
     assert "Products" not in rv.data.decode('utf-8')
@@ -17,3 +22,12 @@ def test_app():
     assert "<h2>Products</h2>" in rv.data.decode('utf-8')
     assert "<li>Apple juice</li>" in rv.data.decode('utf-8')
 
+    rv = web.post('/', data={
+        "product": "Loaf of Bread"
+    })
+    assert rv.status_code == 200
+    #print(rv.data)
+    assert "Shopping List" in rv.data.decode('utf-8')
+    assert "<h2>Products</h2>" in rv.data.decode('utf-8')
+    assert "<li>Apple juice</li>" in rv.data.decode('utf-8')
+    assert "<li>Loaf of Bread</li>" in rv.data.decode('utf-8')
