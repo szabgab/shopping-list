5c5cdb3852573461a8cb21d2eaa52a2be2ac178e
Gabor Szabo <gabor@szabgab.com>
2020-02-13 12:20:41 +0200

    implement remove item

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort, redirect
 import os
 import json
 
@@ -8,6 +8,12 @@ def get_db_file():
     if 'TEST_DB' in os.environ:
         return os.environ['TEST_DB']
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sl.json')
+
+def save(data):
+    db_file = get_db_file()
+    with open(db_file, 'w') as fh:
+        json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
+
 
 @app.route('/', methods=["GET", "POST"])
 def main_post():
@@ -30,10 +36,28 @@ def main_post():
                 'id': data['counter'],
                 'name': product,
             })
-        with open(db_file, 'w') as fh:
-            json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
+        save(data)
 
     return render_template('main.html',
                            title = "Shopping List",
                            products = data['products'],
                     )
+
+@app.route('/remove/<int:idx>', methods=["GET"])
+def remove(idx):
+    db_file = get_db_file()
+    if not os.path.exists(db_file):
+        abort(500)
+
+    with open(db_file) as fh:
+        data = json.load(fh)
+    for ix in range(len(data["products"])):
+        app.logger.info(data["products"][ix]["id"])
+        if data["products"][ix]["id"] == idx:
+            break
+    else:
+        abort(404)
+
+    data["products"].pop(ix)
+    save(data)
+    return redirect('/')
--- test_app.py
+++ test_app.py
@@ -39,3 +39,35 @@ def test_app(tmpdir):
     assert '<li>Apple juice <a href="/remove/1">remove</a></li>' in rv.data.decode('utf-8')
     assert '<li>Loaf of Bread <a href="/remove/2">remove</a></li>' in rv.data.decode('utf-8')
 
+
+def test_remove(tmpdir):
+    print(tmpdir)
+    os.environ['TEST_DB'] = os.path.join(tmpdir, 'temp.db')
+
+    web = app.app.test_client()
+
+    products = ["Milk", "Bread", "Apple juice", "Toothpaste"]
+    for name in products:
+        rv = web.post('/', data={
+            "product": name
+        })
+
+    rv = web.get('/')
+    assert rv.status_code == 200
+    assert "<h2>Products</h2>" in rv.data.decode('utf-8')
+    for idx, name in enumerate(products):
+        assert f'<li>{name} <a href="/remove/{idx+1}">remove</a></li>' in rv.data.decode('utf-8')
+
+
+    rv = web.get('/remove/3')
+    assert rv.status_code == 302
+    print(rv.headers)
+
+    rv = web.get('/')
+    assert rv.status_code == 200
+    assert "<h2>Products</h2>" in rv.data.decode('utf-8')
+    for idx, name in enumerate(products):
+        if idx+1 == 3:
+            assert name not in rv.data.decode('utf-8')
+        else:
+            assert f'<li>{name} <a href="/remove/{idx+1}">remove</a></li>' in rv.data.decode('utf-8')
