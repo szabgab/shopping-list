a79fd2fe9ef98fd371d18c6248d76125ea16917b
Gabor Szabo <gabor@szabgab.com>
2020-02-13 10:17:59 +0200

    allow adding a product. A single product.

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def main():
+def main_get():
     return render_template('main.html',
                            title = "Shopping List",
+                    )
+
+@app.route('/', methods=["POST"])
+def main_post():
+    products = []
+
+    if 'product' in request.form:
+        product = request.form['product']
+        products.append(product)
+
+    return render_template('main.html',
+                           title = "Shopping List",
+                           products = products,
                     )
--- test_app.py
+++ test_app.py
@@ -2,6 +2,18 @@ import app
 
 def test_app():
     web = app.app.test_client()
+
     rv = web.get('/')
     assert rv.status_code == 200
     assert "Shopping List" in rv.data.decode('utf-8')
+    assert "Apple juice" not in rv.data.decode('utf-8')
+    assert "Products" not in rv.data.decode('utf-8')
+
+    rv = web.post('/', data={
+        "product": "Apple juice"
+    })
+    assert rv.status_code == 200
+    assert "Shopping List" in rv.data.decode('utf-8')
+    assert "<h2>Products</h2>" in rv.data.decode('utf-8')
+    assert "<li>Apple juice</li>" in rv.data.decode('utf-8')
+
